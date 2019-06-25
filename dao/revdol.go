a383131c88b965e90407aec9c353bd9a4a1cf8e4
kuhufu <kuhufu@gmail.com>
2019-06-26 00:22:05 +0800

    为 /search/user 的结果添加缓存

--- dao/revdol.go
+++ dao/revdol.go
@@ -1,6 +1,8 @@
 package dao
 
 import (
+	"encoding/json"
+	"github.com/kuhufu/revdol/cache"
 	"github.com/kuhufu/revdol/dao/Interface"
 	"github.com/kuhufu/revdol/dao/mongo"
 )
@@ -68,5 +70,13 @@ func GetAllIdol() interface{} {
 }
 
 func SearchUser(keyWord string) interface{} {
-	return source.SearchUser(keyWord)
+	cacheKeyWord := "search:wd:" + keyWord
+	result, err := cache.GetUnmarshal(cacheKeyWord)
+	if err != nil {
+		data, _ := json.Marshal(source.SearchUser(keyWord))
+		cache.Set(cacheKeyWord, data, 10)
+		result, _ := cache.GetUnmarshal(cacheKeyWord)
+		return result
+	}
+	return result
 }
