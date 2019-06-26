93d7d4de51d2665c5c14c99e94b1b6129e9de988
kuhufu <kuhufu@gmail.com>
2019-06-26 17:01:19 +0800

    改为使用gcache

--- cache/cache.go
+++ cache/cache.go
@@ -1,21 +1,15 @@
 package cache
 
-type cacheStore interface {
-	//expireSeconds <= 0 表示永不过期
-	Set(key string, val []byte, expireSeconds int) error
-
-	Get(key string) (value []byte, err error)
-
-	GetUnmarshal(key string) (value interface{}, err error)
-}
+import "github.com/kuhufu/gcache"
 
 var cacheSize = 100 * 1024 * 1024
 
 //var defaultCache = NewRedisCache(10, "tcp", "127.0.0.1:6379", "")
-var defaultCache = NewMemCache(cacheSize)
+var defaultCache = gcache.NewMemCache(cacheSize)
 
 func Set(key string, val []byte, expireSeconds int) error {
 	return defaultCache.Set(key, val, expireSeconds)
+
 }
 
 func Get(key string) (value []byte, err error) {
