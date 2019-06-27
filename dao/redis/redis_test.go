4f2436f4ba45d1df79cd68ae65f9c75f2e8c09a9
kuhufu <kuhufu@gmail.com>
2019-06-27 21:18:10 +0800

    添加redis的GetForumById的benchmark

--- dao/redis/redis_test.go
+++ dao/redis/redis_test.go
@@ -6,6 +6,13 @@ import (
 )
 
 func TestGetForumById(t *testing.T) {
-	res := GetForumById("1")
-	fmt.Println(string(res))
+	res := GetForumById("17115")
+	fmt.Println(res)
+}
+
+//BenchmarkGetForumById-8   	   10000	    114833 ns/op
+func BenchmarkGetForumById(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetForumById("17115")
+	}
 }
