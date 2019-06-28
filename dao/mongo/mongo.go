d1adbe757a0260139699d97d4b47ddaae86d09db
kuhufu <kuhufu@gmail.com>
2019-06-28 16:19:00 +0800

    加入接口检查

--- dao/mongo/mongo.go
+++ dao/mongo/mongo.go
@@ -1,70 +1,76 @@
 package mongo
 
-type MdbSource int
+import "github.com/kuhufu/revdol/dao/Interface"
 
-func (MdbSource) GetAllForum(params map[string]interface{}) interface{} {
+type Source int
+
+var (
+	_ Interface.Revdol = (Source)(nil)
+)
+
+func (Source) GetAllForum(params map[string]interface{}) interface{} {
 	return GetAllForum(params)
 }
 
-func New() MdbSource {
-	return MdbSource(0)
+func New() Source {
+	return Source(0)
 }
 
-func (MdbSource) GetUserForumCount(id, currentPage int) interface{} {
+func (Source) GetUserForumCount(id, currentPage int) interface{} {
 	return GetUserForumCount(id, currentPage)
 }
 
-func (MdbSource) SearchUser(keyWord string) interface{} {
+func (Source) SearchUser(keyWord string) interface{} {
 	return SearchUser(keyWord)
 }
 
-func (MdbSource) GetIdolForumCount(id int, page int) interface{} {
+func (Source) GetIdolForumCount(id int, page int) interface{} {
 	return GetIdolForumCount(id, page)
 }
 
-func (MdbSource) GetAllIdolForumCount(page int) interface{} {
+func (Source) GetAllIdolForumCount(page int) interface{} {
 	return GetAllIdolForumCount(page)
 }
-func (MdbSource) GetFansNumById(id int, currentPage int) interface{} {
+func (Source) GetFansNumById(id int, currentPage int) interface{} {
 	return GetFansNumById(id, currentPage)
 }
 
-func (MdbSource) GetPopularNumById(id int, currentPage int) interface{} {
+func (Source) GetPopularNumById(id int, currentPage int) interface{} {
 	return GetPopularNumById(id, currentPage)
 }
 
-func (MdbSource) GetAllIdolMeta(currentPage int) interface{} {
+func (Source) GetAllIdolMeta(currentPage int) interface{} {
 	return GetAllIdolMeta(currentPage)
 }
 
-func (MdbSource) GetIdolMetaById(id, page int) interface{} {
+func (Source) GetIdolMetaById(id, page int) interface{} {
 	return GetIdolMetaById(id, page)
 }
 
-func (MdbSource) GetForumById(id int) interface{} {
+func (Source) GetForumById(id int) interface{} {
 	return GetForumById(id)
 }
 
-func (MdbSource) GetAllUser(currentPage int) interface{} {
+func (Source) GetAllUser(currentPage int) interface{} {
 	return GetAllUser(currentPage)
 }
 
-func (MdbSource) GetUserById(id int) interface{} {
+func (Source) GetUserById(id int) interface{} {
 	return GetUserById(id)
 }
 
-func (MdbSource) GetUserContributeById(id int) interface{} {
+func (Source) GetUserContributeById(id int) interface{} {
 	return GetUserContributeById(id)
 }
 
-func (MdbSource) GetAllUserContribute(currentPage int) interface{} {
+func (Source) GetAllUserContribute(currentPage int) interface{} {
 	return GetAllUserContribute(currentPage)
 }
 
-func (MdbSource) GetIdolById(id int) interface{} {
+func (Source) GetIdolById(id int) interface{} {
 	return GetIdolById(id)
 }
 
-func (MdbSource) GetAllIdol() interface{} {
+func (Source) GetAllIdol() interface{} {
 	return GetAllIdol()
 }
